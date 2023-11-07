9352ec4c674711b13a33708c319f85ef307f3cb7
thirdh6o <thirdh6o@foxmail.com>
2023-11-07 22:32:09 +0800

    修改

---
+++ line.cpp
@@ -0,0 +1,37 @@
+#include "Line.h"
+#include <iostream>
+
+/*
+@brief
+*/
+
+void push(LinkedQueue& line){
+
+
+}
+
+//出队
+void pop(LinkedQueue& line){
+
+}
+
+//获取队头
+Patient getHead(LinkedQueue& line){
+
+}
+
+//判断是否空队
+int emptyQueue(LinkedQueue& line){
+
+}
+
+//遍历输出病人信息
+void printQueue(LinkedQueue& line){
+
+}
+
+
+//获取队列长度
+int lengthQueue(LinkedQueue& line){
+    
+}
