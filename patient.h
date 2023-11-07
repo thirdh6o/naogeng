e4eaa72962d1d5d671cdaf1f6bb45c0e44d995b6
thirdh6o <thirdh6o@foxmail.com>
2023-11-07 21:59:43 +0800

    patient头文件完成

--- patient.h
+++ patient.h
@@ -7,8 +7,19 @@ public:
 	std::string ID;
 	int roomNumber;      //科室代号
 	std::string doctor;  //坐诊医生
+     
+	patient(){}
+	patient(std::string name, std::string ID, int roomNumber, std::string doctor, std::string time)
+	{
 
+		this->name = name;
+		this->ID = ID;
+		this->roomNumber = roomNumber;
+		this->doctor = doctor;
+		this->time = time;
+	}
+   //构造函数
+   
+}Patient;
 
-}
-
-#endif;
+#endif
