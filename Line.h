23b9140cbb0e7a818a9f44c1c1e4aab7ebbc9345
thirdh6o <thirdh6o@foxmail.com>
2023-11-07 22:20:58 +0800

    更新

--- Line.h
+++ Line.h
@@ -1,50 +1,53 @@
 #ifndef __LINKEDQUEUE_H__
 #define __LINKEDQUEUE_H__
 #include "patient.h"
+#include <cstddef>
 
-
-//结点结构体类型定义
-typedef struct NodeQueue
+typedef struct linkedqueue
 {
-    ElemType_Queue data;
-    struct NodeQueue* next;
+    Patient patient;
+    linkedqueue* next;
 
-}Node_Queue;
+    linkedqueue(){
+        this->next = nullptr;
+    }
 
-//链表类型定义
-typedef struct Linkedqueue
-{
-    int num;
-    NodeQueue* rear;
-    NodeQueue* front;
-}Linkedqueue;
+    linkedqueue(Patient patient){
+      this->patient = patient;
+      this->next = nullptr;
+    }
 
-//链表初始化
-Linkedqueue* Init_Linkedqueue();
+    linkedqueue(std::string name,std::string ID,int roomNumber,std::string doctor)
+    {
 
-//创建一个新结点
-NodeQueue* Create_New_Node_Queue(ElemType_Queue value);
+        this->patient.name = name;
+        this->patient.ID = ID;
+        this->patient.roomNumber = roomNumber;
+        this->patient.doctor = doctor;
+        this->next = nullptr;
+    }
+
+}LinkedNode, *LinkedQueue;
+
 
 //入队
-void push_Queue(Linkedqueue* linkedqueue, ElemType_Queue value);
+void push(LinkedQueue& line);
 
 //出队
-void pop_Queue(Linkedqueue* linkedqueue);
+void pop(LinkedQueue& line);
 
 //获取队头
-ElemType_Queue get_QueueHead(Linkedqueue* linkedqueue);
+Patient getHead(LinkedQueue& line);
 
 //判断是否空队
-int empty_Queue(Linkedqueue* linkedqueue);
+int emptyQueue(LinkedQueue& line);
 
-//销毁一个队列
-//ForwardListHead *destroy_ForwardListHead(ForwardListHead *forwardlisthead);
-
-void destroy_Queue(Linkedqueue* linkedqueue);
+//遍历输出病人信息
+void printQueue(LinkedQueue& line);
 
 
 //获取队列长度
-int length_Queue(Linkedqueue* linkedqueue);
+int lengthQueue(LinkedQueue& line);
 
 
 #endif
