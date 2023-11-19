02470a0c2c2f6a9c24ca4a9ce66895f0bc8499f7
thirdh6o <thirdh6o@foxmail.com>
2023-11-19 15:04:38 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #include <string>
 #include "Line.h"
+void InitQuene(LinkedQuene& s)
+{
+	QueueNode *p;
+	p = new QueueNode;
+	if (!p)
+	{
+		cout << "·ÖÅäÄÚ´æÊ§°Ü" << endl;
+		return;
+	}
+	p->next = NULL;
+	q.front = q.rear = p;
+}
 
-
+int main() {
+ LinkedQueue patients;
+ InitQuene(patients);
+ 
+ 
+}
